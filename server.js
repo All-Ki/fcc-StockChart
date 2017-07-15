f6df9a1b4f3311ebe4a54a4b9ee93eb8ef070002
All-Ki <k.allegrain@gmail.com>
2017-07-15 19:04:01 +0200

    done

---
+++ server.js
@@ -0,0 +1,94 @@
+var app = require( 'express' )();
+var http = require( 'http' )
+	.Server( app );
+var io = require( 'socket.io' )( http );
+var mongoose = require( 'mongoose' );
+var request = require( 'request' );
+var moment = require( 'moment' )
+mongoose.connect( "mongodb://localhost:27017/stock" );
+var StockController = require( './stocksdb.js' );
+var stockController = new StockController()
+
+
+app.get( '/', ( req, res ) => {
+	res.sendFile( __dirname + '/index.html' );
+} );
+
+app.get( '/getList', ( req, res ) => {
+	stockController.getList( ( data ) => {
+		res.send( data );
+	} )
+} )
+
+app.get( '/getAll', ( req, res ) => {
+	stockController.model.find()
+		.then( ( data ) => {
+			res.json( data );
+		} )
+} )
+
+app.get( '/data/:name', ( req, res ) => {
+	res.send()
+
+} )
+
+
+
+io.on( 'connection', function ( socket ) {
+	var today = moment()
+		.format( 'YYYY-MM-DD' );
+	var oldest = moment()
+		.subtract( 6, 'months' )
+		.format( 'YYYY-MM-DD' )
+	console.log( 'a user connected' );
+	socket.on( 'removeStock', ( name ) => {
+		stockController.removeStock( name, ( err ) => {
+			console.log( err );
+			socket.emit( 'stockRemoved', name );
+		} );
+
+
+	} )
+
+
+	socket.on( 'newStock', ( data ) => {
+
+		request( 'https://www.quandl.com/api/v3/datasets/WIKI/' + data +
+			'/data.json?&start_date=' + oldest + '&end_date=' + today + '&collapse=monthly&' +
+			'column_index=1&api_key=VJjRy6xjDsDtnu6mG6Z-',
+			( err, res, body ) => {
+				body = JSON.parse( body );
+				if ( body.quandl_error ) {
+					if ( body[ 'quandl_error' ][ 'code' ] == 'QECx02' ) {
+						console.log( 'bad stock symbol' );
+
+					}
+				} else {
+					//console.log( data + '\n good stock symbol' +'\n DATA' + JSON.stringify(body)	);
+					stockController.addStock( data, body.dataset_data.data, ( err, item, created ) => {;
+						if ( err ) {
+							console.log( err )
+						} else if ( created ) {
+							var toSend = body.dataset_data.data.map( ( x ) => { return x[ 1 ] } )
+							socket.emit( 'newStockAdded', { name: data, data: toSend } );
+						}
+
+					} )
+				}
+			}
+
+
+		)
+
+	} )
+
+} );
+http.listen( 8080, function () {
+	console.log( 'listening on *:8080' );
+} );
+
+
+
+function formatDate( x ) {
+	return x.slice( 0, 10 );
+}
---
+++ stocksdb.js
@@ -0,0 +1,51 @@
+var mon = require( 'mongoose' );
+var findOrCreate = require( 'mongoose-findorcreate' )
+
+var Schema = mon.Schema;
+
+var stockSchema = new Schema( {
+	name: { type: String, unique: true },
+	dates: [ String ],
+	data: [ String ]
+} );
+stockSchema.plugin( findOrCreate );
+
+var model = mon.model( 'stockSchema', stockSchema );
+
+function stockController() {
+	this.model = model;
+	this.getList = function ( cb ) {
+
+		model.find()
+			.then( ( data ) =>{ data = data.map( ( x ) => { return x.name } );
+       cb( data ) });
+	}
+
+
+
+	this.removeStock = function ( name ,cb) {
+
+		model.findOne( { 'name': name.toString() } ,(err,item)=>{
+      item.remove();
+      cb(err);
+    })
+	}
+
+	this.addStock = function ( name, data,cb ) {
+		console.log( data );
+		var dates = [];
+		var datas = [];
+		for ( var i = 0; i < data.length; i++ ) {
+			dates.push( data[ i ][ 0 ] );
+			datas.push( data[ i ][ 1 ] );
+
+		}
+		model.findOrCreate( { name: name, dates: dates, data: datas },
+			( err,item,created ) => { cb( err,item,created ) } );
+	}
+
+}
+
+
+
+module.exports = stockController;
